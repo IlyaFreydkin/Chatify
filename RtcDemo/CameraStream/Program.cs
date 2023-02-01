730abd1784712173f5deeb2cdf845bdf1e411790
Michael Schletz <schletz@spengergasse.at>
2023-02-01 12:59:24 +0100

    Rework demo sample

--- RtcDemo/CameraStream/Program.cs
+++ RtcDemo/CameraStream/Program.cs
@@ -1,20 +1,30 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using CameraStream.Hubs;
+using CameraStream.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
 
-namespace CameraStream
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddCors(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.AddPolicy("CorsPolicy", policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+builder.Services.AddSignalR();
+builder.Services.AddSingleton<List<User>>();
+builder.Services.AddSingleton<List<Connection>>();
+builder.Services.AddSingleton<List<Call>>();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseCors("CorsPolicy");
+app.UseRouting();
+app.UseAuthorization();
+//app.UseStreamSocket();
+app.MapRazorPages();
+app.MapHub<ConnectionHub>("/cnnctn", options =>
+{
+    options.Transports = Microsoft.AspNetCore.Http.Connections.HttpTransportType.WebSockets;
+});
+app.Run();
