d65ebb54da4bbe718dc58f06888a92cacb853bbd
schletz <schletz@spengergasse.at>
2023-02-05 08:52:12 +0100

    Add signalr + ad login to backend

--- startServer.sh
+++ startServer.sh
@@ -1,11 +1,4 @@
-@echo off
-docker rm -f chatify_sqlserver 2> nul
+docker rm -f chatify_sqlserver &> nul
 docker run -d -p 1433:1433 --name chatify_sqlserver -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=SqlServer2019" mcr.microsoft.com/azure-sql-edge
-IF ERRORLEVEL 1 GOTO error
 dotnet build ChatifyProject\ChatifyProject.Webapi --no-incremental --force
 dotnet watch run -c Debug --project ChatifyProject\ChatifyProject.Webapi
-GOTO end
-
-:error
-PAUSE
-:end
