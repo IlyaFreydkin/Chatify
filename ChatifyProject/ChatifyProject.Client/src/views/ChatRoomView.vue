09cc3a121f11fe3fe4e83b5d808e2299d2c35a87
Richard Liu <LIU22291@spengergasse.at>
2023-05-08 10:01:55 +0200

    chatroom

--- ChatifyProject/ChatifyProject.Client/src/views/ChatRoomView.vue
+++ ChatifyProject/ChatifyProject.Client/src/views/ChatRoomView.vue
@@ -6,59 +6,159 @@ import signalRService from '../services/SignalRService.js';
 </script>
 
 <template>
-    <nav> <NavBar></NavBar> </nav>
-    <div class="chatRoomView">
-        <h5>Chat Room</h5>
-        <p>Mache einen 2. Tab auf und melde dich unter einem anderen User an. Wenn du in der
-            Datei appsettings.Development.json der API deinen User als Suchuser hinterlegt hast,
-            kannst du jedes Passwort verwenden.</p>
-        <div class="newMessage">
-            <textarea v-model="newMessage"></textarea>
-            <button class="btn btn-primary btn-sm" v-on:click="sendMessage()">Senden</button>
+  <div class="chat-room-view">
+    <nav>
+      <NavBar />
+    </nav>
+    <main>
+      <section class="chat-room">
+        <h2 class="chat-room-title">Allgemein</h2>
+        <div class="chat-messages">
+          <div v-for="(message, index) in messages" :key="index" class="chat-message">
+            <div class="avatar">
+              <img src="https://i.imgur.com/8F6yeT2.png" alt="Avatar">
+            </div>
+            <div class="chat-message-content">
+              <div class="chat-message-header">
+                <span class="chat-message-author">Max Mustermann</span>
+                <span class="chat-message-timestamp">vor 5 Minuten</span>
+              </div>
+              <div class="chat-message-text">
+                {{ message }}
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="message" v-for="m in messages" v-bind:key="m">
-            {{ m }}
+        <div class="new-message">
+          <textarea v-model="newMessage" placeholder="Nachricht eingeben"></textarea>
+          <button class="btn btn-primary" @click="sendMessage">Senden</button>
         </div>
-    </div>
-    <footer> <Footer></Footer> </footer>
+      </section>
+    </main>
+    <footer>
+      <Footer />
+    </footer>
+  </div>
 </template>
 
 <style scoped>
-.newMessage {
-    display: flex;
-    gap: 1rem;
-    align-items: flex-end;
-}
-.newMessage textarea {
-    flex-grow: 1;
+
+.chat-room-view {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
 }
 
+nav {
+  margin-bottom: 0.5rem;
+}
+
+.chat-room {
+  background-color: #36393f;
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 0 rgba(4, 4, 5, 0.2);
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  margin: 0.5rem;
+  overflow-y: scroll;
+}
+
+.chat-room-title {
+  color: #fff;
+  font-size: 1.5rem;
+  font-weight: 500;
+  margin: 0;
+  padding: 1rem;
+}
+
+.chat-messages {
+  display: flex;
+  flex-direction: column;
+  margin: 0 1rem 1rem 1rem;
+}
+
+.chat-message {
+  display: flex;
+  margin-bottom: 1rem;
+}
+
+.avatar {
+  height: 36px;
+  margin-right: 0.5rem;
+  width: 36px;
+}
+
+.avatar img {
+  border-radius: 50%;
+  height: 100%;
+  width: 100%;
+}
+
+.chat-message-content {
+  display: flex;
+  flex-direction: column;
+}
+
+.chat-message-header {
+  align-items: center;
+  display: flex;
+  margin-bottom: 0.25rem;
+}
+
+.chat-message-author {
+  color: #fff;
+  font-size: 1rem;
+  font-weight: 500;
+  margin-right: 0.5rem;
+}
+
+.chat-message-timestamp {
+  color: #8e9297;
+  font-size: 0.875rem;
+}
+
+.chat-message-text {
+  background-color: #40444b;
+  border-radius: 0.25rem;
+  color: #fff;
+  font-size: 1rem;
+  padding: 0.75rem;
+}
+
+.new-message {
+  background-color: #2f3136;
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 0 rgba(4, 4, 5, 0.2);
+  display: flex;
+  margin: 0
+}
 </style>
 
 <script>
 export default {
-    data() {
-        return {
-            messages: [],
-            newMessage: ""
-        };
-    },
-    async mounted() {
-        try {
-            signalRService.configureConnection(this.$store.state.userdata.token);
-            signalRService.subscribeEvent('ReceiveMessage', this.onMessageReceive);
-            await signalRService.connect();
-        } catch (e) {
-            alert(JSON.stringify(e));
-        }
-    },
-    methods: {
-        onMessageReceive(message) {
-            this.messages.push(message);
-        },
-        sendMessage() {
-            signalRService.sendMessage(this.newMessage);
-        }
+  data() {
+    return {
+      messages: [],
+      newMessage: "",
+    };
+  },
+  async mounted() {
+    try {
+      signalRService.configureConnection(this.$store.state.userdata.token);
+      signalRService.subscribeEvent("ReceiveMessage", this.onMessageReceive);
+      await signalRService.connect();
+    } catch (e) {
+      alert(JSON.stringify(e));
     }
+  },
+  methods: {
+    onMessageReceive(message) {
+      this.messages.push(message);
+    },
+    sendMessage() {
+      signalRService.sendMessage(this.newMessage);
+    },
+  },
 };
 </script>
