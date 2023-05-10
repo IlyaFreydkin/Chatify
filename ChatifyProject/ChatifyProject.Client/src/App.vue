7b8e37a63d041fb46aaf0db87d2d97694e5d365f
Uros Veljic <vel22675@spengergasse.at>
2023-05-10 15:24:17 +0200

    a

--- ChatifyProject/ChatifyProject.Client/src/App.vue
+++ ChatifyProject/ChatifyProject.Client/src/App.vue
@@ -1,43 +1,18 @@
+
 <script setup>
-import { RouterLink, RouterView } from 'vue-router';
-import NavBar from './components/NavBar.vue';
-import Footer from './components/Footer.vue';
-import WaitingRoomViewVue from './views/WaitingRoomView.vue';
 
 
 </script>
 
 <template>
-  	<nav>
-
-         <NavBar></NavBar>
-        </nav>
-    <div class="appContainer">
-        
-        <main>
-          
-            
-        </main>
+    <div id="app" class="bg-dark">
+    
     </div>
-
-    <footer> <Footer></Footer> </footer> 
-
+    <router-view/>
 </template>
 
-<style scoped>
-html, body {
-  background-color: #2be5bd;
-}
-.appContainer {
-  display: flex;
-  flex-direction: column;
-  max-width: 70em;
-  margin: auto;
-}
-.appContainer nav {
-  flex: 0 0 3em;
-}
-.appContainer main {
-  flex-grow: 1;
-}
+<style>
+
 </style>
+
+
