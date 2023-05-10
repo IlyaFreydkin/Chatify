b7cd9672e42a2b1f1c170d7d8f968f61850c3b92
Uros Veljic <vel22675@spengergasse.at>
2023-05-10 15:36:00 +0200

    änderung

---
+++ ChatifyProject/ChatifyProject.Client/src/views/JournalView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="journalView">
+        <h5>Journal</h5>
+        <p>Hier werden alle User, die gerade eine Verbindung über SignalR haben, aufgelistet.</p>
+    </div>
+</template>
